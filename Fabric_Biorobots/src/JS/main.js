4f8c6e3660896c5b8bae88697f086a931e4908cf
Smolnickovil <SmolnickovWork@gmail.com>
2023-12-20 17:10:34 +0700

    Подключил шрифты

---
+++ Fabric_Biorobots/src/JS/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// const bodyElement = document.body;
+// const childNodes = bodyElement.childNodes;
+// console.log(childNodes);
+
+// const bodyChildren = bodyElement.children;
+// console.log(bodyChildren);
+
+// // поиск по классу 
+// const inputElemetCheck = document.querySelectorAll('.addCoins');
+// console.log(inputElemetCheck);
+
+// const buttonEditAdd = document.querySelector('installBut');
+// console.log(buttonEditAdd);
+
+// const textBut = buttonEditAdd.innerHTML;
+// console.log(textBut);
+
+// buttonEditAdd.innerHTML = 'Insall';
