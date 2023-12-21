1c5574f0ed9f0167e7c0c9d310c799539a963530
Smolnickovil <SmolnickovWork@gmail.com>
2023-12-21 17:17:31 +0700

    В JS подключил кнопку в Header, и checbox

--- Fabric_Biorobots/src/JS/main.js
+++ Fabric_Biorobots/src/JS/main.js
@@ -5,27 +5,63 @@ const quentityCoins = nCoins.textContent;
 const imageCoins = document.querySelector('.coins');//------Вывод монет картинкой
 const addCoins = imageCoins.innerHTML;
 
+const checkbox = document.getElementById('addCheck');//статус чекбокса
+let isChecked = checkbox.checked;
+
+let priceElement;//Изменение кнопки покупки при отутствии денег
+let editInstallBut;
+let editButContent;
+let contentPrEl;
+
+//Изменение кнопки продажи
+// let sellPriceElment;
+let editSellBut;
+let editSellButConten;
+let quentityProductSell;
+let contentQuentProd;
+
+
+
 if(nCoins.textContent == ''){
     nCoins.textContent = 0;
 }
 
 function updateCoins (coinQuen){
   
-  
+
     for(let i = 0; i<= coinQuen; i++){//цикл интерактивно показывающий количество монет
-      if(i == 0){
+        if(i == 0){
         imageCoins.innerHTML = `
         `;
-      } else {
+        } else {
         imageCoins.insertAdjacentHTML(
-          'beforeend',
-          `<img style = "margin-left: ${7 * i}px;  z-index: ${2-i};" class="money n${i}"  src="public/image/money.png" alt="">
-          `
+            'beforeend',
+            `<img style = "margin-left: ${7 * i}px;  z-index: ${2-i};" class="money n${i}"  src="public/image/money.png" alt="">
+            `
         );
-      }
+        }
     }
-  }
+}
 
-  updateCoins(quentityCoins);
+function inpCheck() { //функции checbox
+    if(isChecked == false){
+      isChecked = true;
+    } else if (isChecked == true){
+      isChecked = false;
+    }
+    updateCoins(nCoins.textContent);
+  }  
+
+  //---Скролл кнопи в header
+function navDown(){
+    const moveDown = document.querySelector('.byToCoinsBut');
+    moveDown.scrollIntoView({ 
+      block: "center",
+  
+      behavior: "smooth"
+    });
+  }
+  
+updateCoins(quentityCoins);
 
   
